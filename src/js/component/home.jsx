52699ef59e4a178abd9c8424f321e52ef8ad77bc
Ferreira-Gerson <gersonferreira90@gmail.com>
2022-09-20 13:14:54 +0000

    todo without fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+		//const [todo, setTodo] = useState();
+		const [ task, setTask] = useState([])
+	
+
+
+		const value = (evt) => {
+			if (evt.keyCode === 13){
+				setTask([...task, evt.target.value]);
+				evt.target.value="";
+			}
+
+	};	
+	console.log(value)
+		const removeTodo = (index) => () => {
+			setTask ((item) => (item.filter((_,item) => item !== index)));
+		}
+		const list = task.map((item, index) => {
+			return (
+				<li key={index} onClick={removeTodo(index)}>{item}</li>
+			 )
+			
+		});
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Todo</h1>
+			<ul className="list-group">
+				<li className="list-group-item"><input type="text" placeholder="Please add your task" onKeyDown={value} />{list}</li>
+			</ul>
 		</div>
 	);
 };
